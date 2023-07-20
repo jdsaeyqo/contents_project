f8dd654d8bae91f1a866b980c43cc05536f79759
SANG\jdsae <jdsaeyqi@naver.com>
2023-07-20 13:07:58 +0900

    fix : 프래그먼트 구조 재수정

--- presentation/src/main/java/jdsaeyqi/contents/MainActivity.kt
+++ presentation/src/main/java/jdsaeyqi/contents/MainActivity.kt
@@ -2,40 +2,24 @@ package jdsaeyqi.contents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import jdsaeyqi.contents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initAppBar()
-
     }
 
-    fun initAppBar() {
-        /** 프래그먼트 컨테이너 연결하기 */
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        navController = navHostFragment.navController
-
-        /**
-        액션바 생성
-        nav_graph의 lable에 따라 이름 설정
-         */
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        /** 뒤로 가기 자동으로 추가 */
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
 
 }
