925db25bf01fc016437381d5a89eb1487120ed7f
SANG\jdsae <jdsaeyqi@naver.com>
2023-07-20 13:22:00 +0900

    feat : BottomNavigation 설정

--- presentation/src/main/java/jdsaeyqi/contents/MainActivity.kt
+++ presentation/src/main/java/jdsaeyqi/contents/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import jdsaeyqi.contents.databinding.ActivityMainBinding
@@ -13,11 +14,21 @@ import jdsaeyqi.contents.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initBottomNavigation()
+
+    }
+
+    private fun initBottomNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
 
     }
 
